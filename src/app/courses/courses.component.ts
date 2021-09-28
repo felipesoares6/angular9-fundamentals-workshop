01bbed3b9a221bbebe178ee3b29551d67bde6b57
felipesoares6 <felipeluizsoares@gmail.com>
2021-09-28 13:46:17 -0300

    Display & manage current courser on courses route

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
+  selectedCourse = null;
+
   courses = [
     {
       id: 1,
@@ -13,12 +15,53 @@ export class CoursesComponent implements OnInit {
       description: 'Learn the fundamentals of Angular 9',
       percentComplete: 26,
       favorite: true
+    },
+    {
+      id: 2,
+      title: 'JavaScript the really hard parts',
+      description: 'Lets get our hands dirty with some js',
+      percentComplete: 97,
+      favorite: false
     }
   ];
+
+  selectCourse (course: any) {
+    this.selectedCourse = course;
+  }
+
+  deleteCourse(courseId: number) {
+    const filteredCourses = this.courses.filter(course => course.id !== courseId);
+    this.courses = filteredCourses;
+  }
+
+  resetSelectedCourse() {
+    const emptyCourse = {
+      id: null,
+      title: '',
+      description: '',
+      percentComplete: 0,
+      favorite: false,
+    }
+
+    this.selectedCourse = emptyCourse;
+  }
+
+  cancel() {
+    this.resetSelectedCourse();
+  }
+
+  saveCourse() {
+    this.courses = [
+      ...this.courses,
+    ]
+
+    console.log('save course')
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+    this.resetSelectedCourse();
   }
 
 }
