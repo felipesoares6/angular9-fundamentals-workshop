ed3bcf45bc56a62a63f51d0875ba56e0b1500c2f
felipesoares6 <felipeluizsoares@gmail.com>
2021-09-29 14:14:45 -0300

    Update courses component to have children components & communicate through events

---
+++ src/app/courses/course-details/course-details.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-course-details',
+  templateUrl: './course-details.component.html',
+  styleUrls: ['./course-details.component.scss']
+})
+export class CourseDetailsComponent {
+  selectedCourse;
+  originalTitle;
+
+  @Output() saved = new EventEmitter();
+  @Output() cancelled = new EventEmitter();
+
+  @Input() set course(value) {
+    if (value) {
+      this.selectedCourse = {...value};
+      this.originalTitle = value.title;
+    }
+  };
+}
